5623d8be881e8006ac8da26852c96d59b3f875e4
Bowen Jiang <bowenjiang1993@hotmail.com>
2016-10-15 15:59:50 -0700

    add app.jsx

---
+++ statics/js/app.jsx
@@ -0,0 +1,13 @@
+
+var Student = React.createClass({
+    render: function () {
+        return  <h1>Hello World</h1>
+    }
+});
+
+
+
+
+ReactDOM.render(
+   <Student/>,document.getElementById('app')
+);
